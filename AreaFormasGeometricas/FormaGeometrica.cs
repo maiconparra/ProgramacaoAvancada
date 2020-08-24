66fb9e3c094a0742006bb0b2779bd3ffdfbb1580
marcosxpf95 <marcos2408>
2020-08-24 12:03:55 -0300

    Aula 21/08 - Herança e Polimorfismo.
    Segue código usado para representar a aplicabilidade dos conceitos de Herança e Polimorfimos na Programação Orientada a Objetos.

---
+++ AreaFormasGeometricas/FormaGeometrica.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+namespace AreaFormasGeometricas
+{
+    public abstract class FormaGeometrica
+    {
+        public FormaGeometrica(string _nome, double _profundidade)
+        {
+            this.Nome = _nome;
+            this.Profundidade = _profundidade;
+        }
+
+        public string Nome { get; set; }
+        public double Profundidade { get; set; }
+        public abstract double GetArea();
+        //public abstract double getVolume();
+        public double GetVolume()
+        {
+            double areaFormaGeometrica = GetArea();
+            double volume = areaFormaGeometrica * Profundidade;
+
+            return volume;
+        }
+        public string GetNome()
+        {
+            return Nome;
+        }
+    }
+
+    public class Triangulo : FormaGeometrica
+    {
+        public Triangulo(double _base, double _altura, double _profundidade) : base("Triangulo", _profundidade)
+        {            
+            this.Base = _base;
+            this.Altura = _altura;
+        }
+        public double Base { get; private set; }
+        public double Altura { get; private set; }        
+
+        public override double GetArea()
+        {
+            double areaTrinagulo = (this.Base * Altura) / 2;
+            return areaTrinagulo;
+        }                
+    }
+
+    public class Quadradro : FormaGeometrica
+    {
+        public Quadradro(double _lado, double _profundidade) : base("Quadrado", _profundidade)
+        {
+            this.Lado = _lado;
+        }
+        public double Lado { get; private set; }
+
+        public override double GetArea()
+        {
+            double areaQuadrado = this.Lado * this.Lado;
+            return areaQuadrado;
+        }    
+    }
+
+    public class Retangulo : FormaGeometrica
+    {
+        public Retangulo(double _base, double _altura, double _profundidade) : base("Retangulo", _profundidade)
+        {
+            this.Base = _base;
+            this.Altura = _altura;
+        }
+        public double Base { get; private set; }
+        public double Altura { get; private set; }
+
+        public override double GetArea()
+        {
+            double areaRetangulo = this.Base * this.Altura;
+            return areaRetangulo;
+        }
+    }
+}
---
+++ AreaFormasGeometricas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AreaFormasGeometricas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FormaGeometrica> formasGeometricas = new List<FormaGeometrica>();
+
+            formasGeometricas.Add(new Triangulo(10.0, 10.0, 5));
+            formasGeometricas.Add(new Quadradro(10.0, 5));
+            formasGeometricas.Add(new Retangulo(10.0, 80.0, 5));
+
+            foreach (FormaGeometrica formaGeometrica in formasGeometricas)
+            {
+                Console.WriteLine($"A area do {formaGeometrica.GetNome()} é igual a {formaGeometrica.GetArea()}" +
+                    $"e o volume é igual a {formaGeometrica.GetVolume()}");
+            }
+        }
+    }
+}
